d9dc6083c097dc9edd69cfed5c4b409e5e0300c4
elkCODE <121523529+elk-git@users.noreply.github.com>
2023-03-05 00:06:16 +0100

    init

---
+++ api-countries/client/src/components/home.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+</script>
+
+<template>
+  <div class="home-wrapper">
+    <div class="util">
+      <div class="searchbar">
+        <input type="text" />
+      </div>
+      <div class="filter"></div>
+    </div>
+    <div class="land"></div>
+  </div>
+</template>
